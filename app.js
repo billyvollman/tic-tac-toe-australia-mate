ccda6e40a0c3b22e9215588de96b1aa6fd1be686
billyvollman <b.vollman@gmail.com>
2019-07-11 01:03:08 +1000

    Added flash effect to game for when top row roos win

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var oBoxes = document.querySelectorAll('.o')
 var restart = document.querySelector('.restart')
 var kangaroo = document.querySelector('.x')
 var koala = document.querySelector('.o')
+var tieScore = document.querySelector('.tie.score')
+var player1Score = document.querySelector('.one.score')
 
 
 var turn = 'x';
@@ -15,13 +17,47 @@ var changeBox = function(event) {
          if (turn === 'x') {
             event.target.classList.add('x')
             var kangarooBoxes = document.querySelectorAll('.x')
-            console.log(kangarooBoxes)
-            var kangaroosTop = document.querySelectorAll('.top.x')
-            console.log(kangaroosTop.length)
+            var kangaroosRowTop = document.querySelectorAll('.top.x')
+            var kangaroosRowMiddle = document.querySelectorAll('.middle.x')
+            var kangaroosRowBottom = document.querySelectorAll('.bottom.x')
+            var kangaroosColumnLeft = document.querySelectorAll('.left.x')
+            var kangaroosColumnCenter = document.querySelectorAll('.center.x')
             turn = 'o'
-            // if (kangaroosTop.length === 3) {
-            //     console.log('winner')
-            // }
+            if (kangaroosRowTop.length === 3) {
+                console.log('winner')
+                document.querySelector('.p1').classList.add('flash')
+                kangaroosRowTop.forEach(function(roo){
+                    roo.classList.add('flash')
+                })
+                player1Score.textContent = Number(player1Score.textContent) + 1
+                turn = 'x'
+                restart.style.display = 'block'
+                return
+            } else if (kangaroosRowMiddle.length === 3) {
+                console.log('winner')
+                player1Score.textContent = Number(player1Score.textContent) + 1
+                turn = 'x'
+                restart.style.display = 'block'
+                return
+            } else if (kangaroosRowBottom.length === 3) {
+                console.log('winner')
+                player1Score.textContent = Number(player1Score.textContent) + 1
+                turn = 'x'
+                restart.style.display = 'block'
+                return
+            } else if (kangaroosColumnLeft.length === 3) {
+                console.log('winner')
+                player1Score.textContent = Number(player1Score.textContent) + 1
+                turn = 'x'
+                restart.style.display = 'block'
+                return
+            } else if (kangaroosColumnCenter.length === 3) {
+                console.log('winner')
+                player1Score.textContent = Number(player1Score.textContent) + 1
+                turn = 'x'
+                restart.style.display = 'block'
+                return
+            } 
         } else if (turn === 'o' ) {
             event.target.classList.add('o')
             turn = 'x'
@@ -33,15 +69,8 @@ var changeBox = function(event) {
         console.log('time to reset')
         turn = 'x'
         restart.style.display = 'block'
-
-        var clearBoxes = function() {
-            restart.style.display = 'none'
-            boxes.forEach(function(animal) {
-                animal.classList.remove('x', 'o')
-                // koala.classList.remove('o')
-            })
-        }
-        restart.addEventListener('click', clearBoxes)
+        tieScore.textContent = Number(tieScore.textContent) + 1
+        return
 
     }
     // event.target.classList.add('o')
@@ -51,23 +80,12 @@ boxes.forEach(function(box) {
     box.addEventListener('click', changeBox)
 })
 
-
-
-
-// if (turn === 'x') {
-    
-// } else if (turn === 'o') {
-//     var changeBox = function(event) {
-//         debugger
-//         console.log('click is working')
-//         event.target.classList.add('o')
-//         turn = 'x'
-//         // event.target.classList.add('o')
-//     }
-    
-//     boxes.forEach(function(box) {
-//         box.addEventListener('click', changeBox)
-//     })
-// }
-
+var clearBoxes = function() {
+    restart.style.display = 'none'
+    boxes.forEach(function(animal) {
+        animal.classList.remove('x', 'o', 'flash')
+        // koala.classList.remove('o')
+    })
+}
+restart.addEventListener('click', clearBoxes)
 
